33a7ad40fb37f01d71a805e8dec88e787f2d46d9
sycured <60801403+sycured@users.noreply.github.com>
2020-05-11 08:55:58 -0500

    run brittany

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -8,10 +8,10 @@
 
 module Foundation where
 
-import Import.NoFoundation
-import Control.Monad.Logger        (LogSource)
-import Yesod.Core.Types            (Logger)
-import qualified Yesod.Core.Unsafe as Unsafe
+import           Import.NoFoundation
+import           Control.Monad.Logger           ( LogSource )
+import           Yesod.Core.Types               ( Logger )
+import qualified Yesod.Core.Unsafe             as Unsafe
 
 -- | The foundation datatype for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
@@ -42,47 +42,48 @@ mkYesodData "App" $(parseRoutesFile "config/routes")
 instance Yesod App where
     -- Controls the base of generated URLs. For more information on modifying,
     -- see: https://github.com/yesodweb/yesod/wiki/Overriding-approot
-    approot :: Approot App
-    approot = ApprootRequest $ \app req ->
-        case appRoot $ appSettings app of
-            Nothing -> getApprootText guessApproot app req
-            Just root -> root
+  approot :: Approot App
+  approot = ApprootRequest $ \app req -> case appRoot $ appSettings app of
+    Nothing   -> getApprootText guessApproot app req
+    Just root -> root
 
-    -- Store session data on the client in encrypted cookies,
-    -- default session idle timeout is 120 minutes
-    makeSessionBackend :: App -> IO (Maybe SessionBackend)
-    makeSessionBackend _ = Just <$> defaultClientSessionBackend
-        120    -- timeout in minutes
-        "config/client_session_key.aes"
+  -- Store session data on the client in encrypted cookies,
+  -- default session idle timeout is 120 minutes
+  makeSessionBackend :: App -> IO (Maybe SessionBackend)
+  makeSessionBackend _ = Just <$> defaultClientSessionBackend
+    120    -- timeout in minutes
+    "config/client_session_key.aes"
 
-    -- Yesod Middleware allows you to run code before and after each handler function.
-    -- The defaultYesodMiddleware adds the response header "Vary: Accept, Accept-Language" and performs authorization checks.
-    -- Some users may also want to add the defaultCsrfMiddleware, which:
-    --   a) Sets a cookie with a CSRF token in it.
-    --   b) Validates that incoming write requests include that token in either a header or POST parameter.
-    -- To add it, chain it together with the defaultMiddleware: yesodMiddleware = defaultYesodMiddleware . defaultCsrfMiddleware
-    -- For details, see the CSRF documentation in the Yesod.Core.Handler module of the yesod-core package.
-    yesodMiddleware :: ToTypedContent res => Handler res -> Handler res
-    yesodMiddleware = defaultYesodMiddleware
+  -- Yesod Middleware allows you to run code before and after each handler function.
+  -- The defaultYesodMiddleware adds the response header "Vary: Accept, Accept-Language" and performs authorization checks.
+  -- Some users may also want to add the defaultCsrfMiddleware, which:
+  --   a) Sets a cookie with a CSRF token in it.
+  --   b) Validates that incoming write requests include that token in either a header or POST parameter.
+  -- To add it, chain it together with the defaultMiddleware: yesodMiddleware = defaultYesodMiddleware . defaultCsrfMiddleware
+  -- For details, see the CSRF documentation in the Yesod.Core.Handler module of the yesod-core package.
+  yesodMiddleware :: ToTypedContent res => Handler res -> Handler res
+  yesodMiddleware = defaultYesodMiddleware
 
-    -- What messages should be logged. The following includes all messages when
-    -- in development, and warnings and errors in production.
-    shouldLogIO :: App -> LogSource -> LogLevel -> IO Bool
-    shouldLogIO app _source level =
-        return $
-        appShouldLogAll (appSettings app)
-            || level == LevelWarn
-            || level == LevelError
+  -- What messages should be logged. The following includes all messages when
+  -- in development, and warnings and errors in production.
+  shouldLogIO :: App -> LogSource -> LogLevel -> IO Bool
+  shouldLogIO app _source level =
+    return
+      $  appShouldLogAll (appSettings app)
+      || level
+      == LevelWarn
+      || level
+      == LevelError
 
-    makeLogger :: App -> IO Logger
-    makeLogger = return . appLogger
+  makeLogger :: App -> IO Logger
+  makeLogger = return . appLogger
 
 -- Useful when writing code that is re-usable outside of the Handler context.
 -- An example is background jobs that send email.
 -- This can also be useful for writing code that works across multiple Yesod applications.
 instance HasHttpManager App where
-    getHttpManager :: App -> Manager
-    getHttpManager = appHttpManager
+  getHttpManager :: App -> Manager
+  getHttpManager = appHttpManager
 
 unsafeHandler :: App -> Handler a -> IO a
 unsafeHandler = Unsafe.fakeHandlerGetLogger appLogger
