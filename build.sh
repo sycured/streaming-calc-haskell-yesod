6ce3d902106935c368a39ad603301ec42320475a
sycured <60801403+sycured@users.noreply.github.com>
2020-09-21 01:06:28 -0500

    install yum + migrate from dnf to yum

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -xe
 
 mkbuild=$(buildah from -v "$TRAVIS_BUILD_DIR":"$mntbuild"/home/hsk/to_build ghcr.io/sycured/centos-haskell-builder:latest)
 mntbuild=$(buildah mount "$mkbuild")
-buildah run --user root "$mkbuild" bash -c "chown -R hsk /home/hsk/to_build && dnf install zlib-devel -y"
+buildah run --user root "$mkbuild" bash -c "chown -R hsk /home/hsk/to_build && yum install zlib-devel -y"
 buildah run "$mkbuild" -- bash -c "cd /home/hsk/to_build && stack setup && stack build --ghc-options='-fPIC -optl-pthread' --test --copy-bins"
 
 mkimg=$(buildah from scratch)
@@ -14,7 +14,7 @@ buildah config --port 3000 "$mkimg"
 buildah config --workingdir='/opt' "$mkimg"
 buildah config --cmd '/opt/streaming-calc-haskell-yesod' "$mkimg"
 mntimg=$(buildah mount "$mkimg")
-dnf --nogpgcheck --installroot="$mkimg" --repofrompath centos8,http://mirror.centos.org/centos/8/BaseOS/x86_64/os/ --repo=centos8 install glibc-langpack-en gmp zlib -y
+yum --nogpgcheck --installroot="$mkimg" --repofrompath centos8,http://mirror.centos.org/centos/8/BaseOS/x86_64/os/ --repo=centos8 install glibc-langpack-en gmp zlib -y
 mkdir -p "$mntimg"/opt/config "$mntimg"/opt/static
 cp "$mntbuild"/home/hsk/.local/bin/streaming-calc-haskell-yesod "$mntimg"/opt/
 buildah unmount "$mkimg"
